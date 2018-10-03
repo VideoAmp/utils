7a8aed1738930688f87a51fdcf57c921bbc18577
Alex Bohr <abohr@videoamp.com>
2018-10-03 14:44:58 -0700

    prod version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hoidn_utils',
-    version='0.0.2.dev1',
+    version='0.0.2',
     description='Some util code forked for Oliver Hoidn''s repo, mostly for the caching decorators',
     packages=find_packages('.'),
     install_requires=['plotly', 'numpy', 'dill', 'meta', 'pathos'],
