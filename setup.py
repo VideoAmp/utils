50baca7666ad6769aa498ee31faba040b49c3515
Alex Bohr <abohr@videoamp.com>
2018-10-03 10:49:54 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hoidn.utils',
-    version='0.0.1.dev1',
+    version='0.0.1.dev2',
     description='Some util code forked for Oliver Hoidn''s repo, mostly for the caching decorators',
     packages=find_packages('.'),
     install_requires=['plotly', 'meta', 'pathos'],
