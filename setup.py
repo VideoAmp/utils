c242a16fd356c895346b55034f6ced95f8902d6c
Alex Bohr <abohr@videoamp.com>
2018-10-03 10:49:02 -0700

    adding version and rename module

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name = 'utils',
-    packages = find_packages('.'),
-    package_dir = {'utils': 'utils'},
-    install_requires = ['plotly', 'meta', 'pathos'],
-    zip_safe = False)
+setup(
+    name='hoidn.utils',
+    version='0.0.1.dev1',
+    description='Some util code forked for Oliver Hoidn''s repo, mostly for the caching decorators',
+    packages=find_packages('.'),
+    install_requires=['plotly', 'meta', 'pathos'],
+    zip_safe=False
+)
